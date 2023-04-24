f5fc8cd3308e5ba96836484a0f963adb296c60e6
mollyb14 <128402937+mollyb14@users.noreply.github.com>
2023-04-24 12:15:23 +0200

    Update basicMeaningExample.R

--- basicMeaningExample.R
+++ basicMeaningExample.R
@@ -1,19 +1,19 @@
 #load table
-dataMeaning=read.table('VideoEssayLength', header=T, sep="\t")
+dataMeaning=read.table('TheatrePopulation.txt', header=T, sep="\t")
 
 #plot with the labels
-plot(dataMeaning$length,dataMeaning$title,type="n",xlab="year,ylab="title")
-text(dataMeaning$length,dataMeaning$meanings,dataMeaning$entry, cex= 1)
+plot(dataMeaning$theatres,dataMeaning$population,type="n",xlab="number of theatres",ylab="number of people")
+text(dataMeaning$theatres,dataMeaning$population,dataMeaning$state, cex= 1)
 
 #Now we'd like to build a regression model, i.e., find the equation for the straight line that best defines the dataset. 
 
-myModel = lm(dataMeaning$meanings~dataMeaning$length)
+myModel = lm(dataMeaning$theatres~dataMeaning$population)
 summary(myModel)
 #least square fitting
 abline(myModel,col="red",lw="3")
 
 #quick and dirty trick to avoid label overlap (best solution: export to
 # vector format, like eps, and edit in separate software)
-newMeanings = jitter(dataMeaning$meanings, 5)
-plot(dataMeaning$length,newMeanings,type="n",xlab="number of letters",ylab="number of meanings")
-text(dataMeaning$length,newMeanings,dataMeaning$entry, cex= 1)
+newMeanings = jitter(dataMeaning$theatres, 5)
+plot(dataMeaning$theatres,newMeanings,type="n",xlab="number of theatres",ylab="number of people")
+text(dataMeaning$theatres,newMeanings,dataMeaning$state, cex= 1)
