77a55a5c176e8b79377a601c1b450c3f7b39bacc
mollyb14 <128402937+mollyb14@users.noreply.github.com>
2023-04-24 12:26:39 +0200

    Update basicTheatrePopulation.R

--- basicTheatrePopulation.R
+++ basicTheatrePopulation.R
@@ -2,18 +2,18 @@
 dataMeaning=read.table('TheatrePopulation.txt', header=T, sep="\t")
 
 #plot with the labels
-plot(dataMeaning$theatres,dataMeaning$population,type="n",xlab="number of theatres",ylab="number of people")
+plot(dataMeaning$theatres,dataMeaning$people,type="n",xlab="number of theatres",ylab="number of people")
 text(dataMeaning$theatres,dataMeaning$population,dataMeaning$state, cex= 1)
 
 #Now we'd like to build a regression model, i.e., find the equation for the straight line that best defines the dataset. 
 
-myModel = lm(dataMeaning$theatres~dataMeaning$population)
+myModel = lm(dataMeaning$population~dataMeaning$theatres)
 summary(myModel)
 #least square fitting
 abline(myModel,col="red",lw="3")
 
 #quick and dirty trick to avoid label overlap (best solution: export to
 # vector format, like eps, and edit in separate software)
-newMeanings = jitter(dataMeaning$theatres, 5)
+newMeanings = jitter(dataMeaning$population, 5)
 plot(dataMeaning$theatres,newMeanings,type="n",xlab="number of theatres",ylab="number of people")
 text(dataMeaning$theatres,newMeanings,dataMeaning$state, cex= 1)
